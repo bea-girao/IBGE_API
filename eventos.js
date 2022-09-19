c68d122f46ebd6aaaf5ae4aa7320d8d09eb0c26b
catheali <aliciacatherinealencar@gmail.com>
2022-09-19 19:40:27 -0300

    cidade e estado preenchidos

--- eventos.js
+++ eventos.js
@@ -1,5 +1,6 @@
 const URL_REGIOES = 'https://servicodados.ibge.gov.br/api/v1/localidades/regioes';
 const URL_ESTADOS = (id) => `https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${id}/estados`;
+const URL_MUNICIPIOS = (id) => `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${id}/municipios`;
 
 fetch(URL_REGIOES)
     .then(response => response.json())
@@ -16,6 +17,17 @@ regiao.addEventListener('change', () => {
     .then(data => {
         data.map(cadaResultado => {
             estado.innerHTML += `<option value="${cadaResultado.id}">${cadaResultado.nome}</option>`;
+        });
+    });
+});
+
+estado.addEventListener('change', () => {
+    cidade.innerHTML = `<option>-- SELECIONE --</option>`;
+    fetch(URL_MUNICIPIOS(estado.value))
+    .then(response => response.json())
+    .then(data => {
+        data.map(cadaResultado => {
+            cidade.innerHTML += `<option value="${cadaResultado.id}">${cadaResultado.nome}</option>`
         });
     });
 });
